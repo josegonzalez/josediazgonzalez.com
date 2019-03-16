7f396a7b321f4ae5a962b42b4ff17250af91d6ee
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-03-16 18:56:16 -0400

    hack: get the maintainer from the GITHUB_REPOSITORY env var

--- action-build/entrypoint.sh
+++ action-build/entrypoint.sh
@@ -2,7 +2,8 @@
 set -eo pipefail
 
 main() {
-  local REPOSITORY_URL="https://${GITHUB_TOKEN}@github.com/${SITE_REPOSITORY}.git"
+  local MAINTAINER="$(GITHUB_REPOSITORY | cut -d '/' -f1)"
+  local REPOSITORY_URL="https://${MAINTAINER}:${GITHUB_TOKEN}@github.com/${SITE_REPOSITORY}.git"
 
   echo "-----> Cloning site repository"
   git clone "$REPOSITORY_URL" _site > /dev/null 2>&1 | sed "s/^/       /"
@@ -16,9 +17,10 @@ main() {
   git config user.name "$GITHUB_ACTOR" | sed "s/^/       /"
   git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" | sed "s/^/       /"
 
-  echo "-----> Pushing code"
+  echo "-----> Committing changes"
   git add . | sed "s/^/       /"
   git commit -m 'jekyll build from Action' | sed "s/^/       /"
+  echo "-----> Pushing code"
   git push origin master | sed "s/^/       /"
 
   popd > /dev/null
