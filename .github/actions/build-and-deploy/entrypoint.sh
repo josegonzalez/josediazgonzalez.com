321d1bb80ac25a2635fa5c1fcb653abbc9d83c9a
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-11-14 05:59:23 -0500

    fix: push to correct repository

--- .github/actions/build-and-deploy/entrypoint.sh
+++ .github/actions/build-and-deploy/entrypoint.sh
@@ -3,22 +3,32 @@ set -eo pipefail
 
 main() {
   local MAINTAINER="$(echo "$GITHUB_REPOSITORY" | cut -d '/' -f1)"
-  local REPOSITORY_URL="https://${MAINTAINER}:${JEKYLL_GITHUB_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  local REPOSITORY_URL="https://${MAINTAINER}:${JEKYLL_GITHUB_ACCESS_TOKEN}@github.com/${SITE_REPOSITORY}.git"
 
   if [[ -z "$JEKYLL_GITHUB_ACCESS_TOKEN" ]]; then
     echo "::error file=entrypoint.sh,line=8,col=2::Missing JEKYLL_GITHUB_ACCESS_TOKEN"
     return 1
   fi
 
+  if [[ -z "$SITE_REPOSITORY" ]]; then
+    echo "::error file=entrypoint.sh,line=13,col=2::Missing SITE_REPOSITORY"
+    return 1
+  fi
+
+  echo "-----> Cloning site repository"
+  git clone "$REPOSITORY_URL" _site > /dev/null 2>&1 | sed "s/^/       /"
+
   echo "-----> Building site"
   jekyll build | sed "s/^/       /"
+
+  pushd _site > /dev/null
 
   echo "-----> Configuring git for push"
   git config user.name "$GITHUB_ACTOR" | sed "s/^/       /"
   git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" | sed "s/^/       /"
 
   echo "-----> Committing changes"
-  git add _site | sed "s/^/       /"
+  git add . | sed "s/^/       /"
   git commit -m 'Jekyll Build from Action' | sed "s/^/       /"
 
   echo "-----> Pushing code"
