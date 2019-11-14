26ae23751805d4be9554c09b42e2bed97d814774
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2019-11-14 05:50:07 -0500

    fix: correct build context

--- .github/actions/build-and-deploy/entrypoint.sh
+++ .github/actions/build-and-deploy/entrypoint.sh
@@ -10,21 +10,17 @@ main() {
     return 1
   fi
 
-  echo "-----> Cloning site repository"
-  git clone "$REPOSITORY_URL" _site > /dev/null 2>&1 | sed "s/^/       /"
-
   echo "-----> Building site"
   jekyll build | sed "s/^/       /"
-
-  pushd _site > /dev/null
 
   echo "-----> Configuring git for push"
   git config user.name "$GITHUB_ACTOR" | sed "s/^/       /"
   git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" | sed "s/^/       /"
 
   echo "-----> Committing changes"
-  git add . | sed "s/^/       /"
-  git commit -m 'jekyll build from Action' | sed "s/^/       /"
+  git add _site | sed "s/^/       /"
+  git commit -m 'Jekyll Build from Action' | sed "s/^/       /"
+
   echo "-----> Pushing code"
   git push origin master | sed "s/^/       /"
 
